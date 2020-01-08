3ff233bdfcdc331fb08fd0399213748aa0237e18
Cuadrix <51061675+Cuadrix@users.noreply.github.com>
2020-01-08 03:41:20 +0100

    Added lookup method

--- puppeteer-page-proxy.js
+++ puppeteer-page-proxy.js
@@ -113,7 +113,7 @@ class RequestUtil {
 }
 
 // REF: https://github.com/xiyuan-fengyu/ppspider/blob/master/src/puppeteer/PuppeteerUtil.ts
-module.exports = async function useProxy(page, proxy, enableCache = true) {
+module.exports = async function(page, proxy, enableCache = true) {
     page["_proxy"] = proxy;
     page["_enableCacheInProxy"] = enableCache;
     await page.setRequestInterception(true);
@@ -268,5 +268,48 @@ module.exports = async function useProxy(page, proxy, enableCache = true) {
             }
         };
         page.on("request", _proxyHandler);
+    }
+}
+
+module.exports.lookup = async function(page, lookupService = "https://api.ipify.org?format=json", isJSON = true, timeout = 30000) {
+    // Executes clientside code to request data from a lookup service
+    const XMLHttpRequest = async () => {
+        return await page.evaluate((lookupService, timeout, isJSON) => {
+            return new Promise(resolve => {
+                var req = new XMLHttpRequest();
+                req.open("GET", lookupService, true);
+                req.timeout = timeout;
+                req.onload = () => {
+                    if (req.status >= 200 && req.status <= 299) {
+                        resolve(isJSON ? JSON.parse(req.responseText) : req.responseText);
+                    } else {
+                        resolve(xhrFailed(`Request from [${window.location.href.slice(0, -1)}] to [${lookupService}] failed with status code [${req.status}]`));
+                    }
+                };
+                req.ontimeout = e => {
+                    resolve(xhrFailed(`Request from [${window.location.href.slice(0, -1)}] to [${lookupService}] timed out -> [${req.timeout} ms]`));
+                };
+                req.send();
+            });
+        }, lookupService, timeout, isJSON);
+    };
+    try {
+        // Bypass Content Security Policy to be able to make requests to other sites
+        await page.setBypassCSP(true);
+        const errName = "xhrFailed";
+        // Check if function has already been exposed to avoid errors
+        // Calls this function if the client side request fails or times out
+        if (!page._pageBindings.has(errName)) {
+            await page.exposeFunction(errName, reason => {
+                console.error(reason);
+                return
+            });
+        }
+        return await XMLHttpRequest();
+    } catch(error) {
+        // If failed due to navigation to another page, try again in the new execution context
+        if (error.message === "Execution context was destroyed, most likely because of a navigation." || error.message === "Execution context was destroyed.") {
+            return await XMLHttpRequest();
+        }
     }
 }
