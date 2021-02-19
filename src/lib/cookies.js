60563e177d1bb609a7659bb8641b308f1cd2412c
Hossein Moradi Davijani <moradidavijani@gmail.com>
2021-02-19 19:45:29 +0330

    Removed optional chaining syntax because of an error in our CI

--- src/lib/cookies.js
+++ src/lib/cookies.js
@@ -64,7 +64,7 @@ const formatCookie = (cookie) => {
 class CookieHandler extends CDP {
     constructor(request) {
         super(request._client);
-        this.url = request.isNavigationRequest() ? request.url() : request.frame()?.url() || request.url();
+        this.url = request.isNavigationRequest() || !request.frame() ? request.url() : request.frame().url();
         this.domain = new URL(this.url).hostname;
     }
     // Parse an array of raw cookies to an array of cookie objects
